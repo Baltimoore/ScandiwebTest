dd9a6206f45259922d50451ccc2a64a525ac5c1f
baltimoore <gi17001@students.lu.lv>
2022-03-09 09:49:55 +0200

    sākums mājaslapas redirectiem

---
+++ php/viewAdd.php
@@ -0,0 +1,63 @@
+<?php
+define("TITLE", "Product Add");
+define("BUTTON_1", 'id="save-product-btn" onclick="">Save');
+define("BUTTON_2", 'id="cancel-product-btn" onclick="">Cancel');
+// Lai būtu iespējams ievietot mainīgos paraugā
+ob_start();
+require(dirname(__DIR__, 1) . "/html/viewHeader.html");
+ob_end_flush();
+?>
+
+<div id="product_form" class="container bodies">
+    <div class="row">
+        <label for="sku" class="col-1">SKU</label>
+        <input id="sku" class="col-2" type="text" required>
+    </div>
+    <div class="row">
+        <label for="name" class="col-1">Name</label>
+        <input id="name" class="col-2" type="text" required>
+    </div>
+    <div class="row">
+        <label for="price" class="col-1">Price ($)</label>
+        <input id="price" class="col-2" type="text" required>
+    </div>
+    <div class="row">
+        <label for="productType" class="col-2">Type Switcher</label>
+        <select id="productType" class="form-select" required>
+            <option selected disabled>Type Switcher</option>
+            <option value="1">DVD</option>
+            <option value="2">Book</option>
+            <option value="3">Furniture</option>
+        </select>
+    </div>
+
+    <script src="../js/typeInput.js"></script>
+
+    <div id="itemProperties" display="none">
+        <div id="DVD" class="row">
+            <label for="size" class="col-1">Size (MB)</label>
+            <input id="size" class="col-2" type="text" required>
+        </div>
+        <div id="Book" class="row">
+            <label for="weight" class="col-1">Weight (KG)</label>
+            <input id="weight" class="col-2" type="text" required>
+        </div>
+        <div id="Furniture">
+            <div class="row">
+                <label for="height" class="col-1">Height (CM)</label>
+                <input id="height" class="col-2" type="text" required>
+            </div>
+            <div class="row">
+                <label for="width" class="col-1">Width (CM)</label>
+                <input id="width" class="col-2" type="text" required>
+            </div>
+            <div class="row">
+                <label for="length" class="col-1">Length (CM)</label>
+                <input id="length" class="col-2" type="text" required>
+            </div>
+        </div>
+        <p id="helpInfo"></p>
+    </div>
+
+</div>
+<?= require(dirname(__DIR__) . "/html/viewFooter.html"); ?>
--- php/viewList.php
+++ php/viewList.php
@@ -2,15 +2,14 @@
 define("TITLE", "Product List");
 define("BUTTON_1", 'id="add-product-btn" onclick="">ADD');
 define("BUTTON_2", 'id="delete-product-btn" onclick="">MASS DELETE');
-
+// Lai būtu iespējams ievietot mainīgos paraugā
 ob_start();
 require(dirname(__DIR__, 1) . "/html/viewHeader.html");
 ob_end_flush();
 ?>
 
 <div id="body" class="container">
-<div class="row row-cols-auto">
-<?php 
+    <div class="row row-cols-auto"><?php 
     $itemCount = 1; //kkāds inventāra skaita lasīšanas skripts no datubāzes
     for ($i=0; $i < $itemCount; $i++) {
         //visi mainīgie apakšā lasa no klases vai kkā tāda
@@ -21,7 +20,9 @@ ob_end_flush();
         $attributeValue = "15";
         $attributeMeasure = "MB";
         // Card, kuras info tiek aizpildīts ar klases pasniegtajiem datiem
-        echo '<div class="col card border-dark">
+        // Echo atdalīts tukšā rindā, lai kad lietotājs skatās mājaslapas kodu, nav briesmīgs noformējums
+        echo '
+        <div class="col card border-dark">
             <input class="form-check-input delete-checkbox" type="checkbox" value="" aria-label="Select for deletion">
             <div class="cardContents text-center">
                 <p class="sku text-muted">' . $sku . '</p>
@@ -33,9 +34,9 @@ ob_end_flush();
                     <span class="attributeMeasure">' . $attributeMeasure . '</span>
                 </p>
             </div>
-        </div>' . "\n";
-    }
-?>
+        </div>';
+    };
+    echo "\n";?>
     </div>
 </div>
 <?= require(dirname(__DIR__, 1) . "/html/viewFooter.html"); ?>
