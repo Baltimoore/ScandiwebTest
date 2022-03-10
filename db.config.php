546968d2c8d5687ca8440380c3ceca55fe087502
baltimoore <gi17001@students.lu.lv>
2022-03-10 15:10:55 +0200

    strādājošs? list skata aizpildītājs

--- db.config.php
+++ db.config.php
@@ -21,24 +21,81 @@ class item
     protected $sku;
     protected $name;
     protected $price;
+    protected $value;
+    protected $type;
+
+    function setSKU($new)
+    {
+        $this->sku = $new;
+    }
+    function setName($new)
+    {
+        $this->name = $new;
+    }
+    function setPrice($new)
+    {
+        $this->price = $new;
+    }
+    function setValue($new)
+    {
+        $this->value = $new;
+    }
+    function setType($new)
+    {
+        $this->type = $new;
+    }
+
+    function readSKU()
+    {
+        return $this->sku;
+    }
+    function readName()
+    {
+        return $this->name;
+    }
+    function readPrice()
+    {
+        return $this->price;
+    }
+    function readValue()
+    {
+        return $this->value;
+    }
+    function readType()
+    {
+        return $this->type;
+    }
 }
-class book extends item
+
+class type
 {
-    public $attributeType = "Weight";
-    protected $weight;
-    public $attributeMeasure = "kg";
+    protected $id;
+    protected $valueTitle;
+    protected $valueMeasure;
+
+    function setId($new)
+    {
+        $this->id = $new;
+    }
+    function setValueTitle($new)
+    {
+        $this->valueTitle = $new;
+    }
+    function setValueMeasure($new)
+    {
+        $this->valueMeasure = $new;
+    }
+
+    function readId()
+    {
+        return $this->id;
+    }
+    function readValueTitle()
+    {
+        return $this->valueTitle;
+    }
+    function readValueMeasure()
+    {
+        return $this->valueMeasure;
+    }
 }
-class dvd extends item
-{
-    public $attributeType = "Size";
-    protected $size;
-    public $attributeMeasure = "GB";
-}
-class furniture extends item
-{
-    public $attributeType = "Dimensions";
-    protected $height;
-    protected $width;
-    protected $length;
-    public $attributeMeasure = "CM";
-};
--- views/viewList.php
+++ views/viewList.php
@@ -11,27 +11,37 @@ ob_end_flush();
 
 <div id="content" class="container">
     <div class="row row-cols-auto"><?php 
-    $itemCount = 1; //kkāds inventāra skaita lasīšanas skripts no datubāzes
-    for ($i=0; $i < $itemCount; $i++) {
-        //visi mainīgie apakšā lasa no klases vai kkā tāda
-        $sku = "SGRE15672326";
-        $name = "Yuri";
-        $price = "69.69";
-        $attributeType = "DVD";
-        $attributeValue = "15";
-        $attributeMeasure = "MB";
+    // Nolasām produktu tipu datus no datubāzes, un ievietojam tos masīvā
+    $types[] = new type();
+
+    // Nolasām produktu datus no datubāzes, un ievietojam tos masīvā
+    $products[] = new item();
+    $sql = "SELECT * FROM inventory;";
+    $info = mysqli_query($connection, $sql);
+    //kamēr ir kāda rinda
+    $i = 0;
+    while($row = mysqli_fetch_assoc($info)) {
+        $products[$i]->setSKU($row['SKU']);
+        $products[$i]->setName($row['Name']);
+        $products[$i]->setPrice($row['Price']);
+        $products[$i]->setValue($row['Type']);
+        $products[$i]->setType($row['Value']);
+        $i++;
+    }
+
+    for ($i=0; $i < sizeof($products); $i++) {
         // Card, kuras info tiek aizpildīts ar klases pasniegtajiem datiem
         // Echo atdalīts tukšā rindā, lai kad lietotājs skatās mājaslapas kodu, nav briesmīgs noformējums
         echo '
         <div class="col card border-dark">
             <input class="form-check-input delete-checkbox" type="checkbox" value="" aria-label="Select for deletion">
             <div class="cardContents text-center">
-                <p class="sku text-muted">' . $sku . '</p>
-                <p class="name">' . $name . '</p>
-                <p class="price">' . $price . '</p>
+                <p class="sku text-muted">' . $products[$i]->readSKU() . '</p>
+                <p class="name">' . $products[$i]->readName() . '</p>
+                <p class="price">' . $products[$i]->readPrice() . '</p>
                 <p class="attributes">
-                    <span class="attributeType">' . $attributeType . ': </span>
-                    <span class="attributeValue">' . $attributeValue . '</span>
+                    <span class="attributeType">' . $products[$i]->readType() . ': </span>
+                    <span class="attributeValue">' . $products[$i]->readValue() . '</span>
                     <span class="attributeMeasure">' . $attributeMeasure . '</span>
                 </p>
             </div>
